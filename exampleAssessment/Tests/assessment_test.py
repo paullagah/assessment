bff0a22624e14f16123f26131ebd513634db1912
paullagah <plagah@outlook.com>
2020-07-02 10:24:53 +0100

    Changes to folder

--- exampleAssessment/Tests/assessment_test.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from Code import example
-
-def test_endsPy():
-    assert example.endsPy("ilovepy") == True
-    assert example.endsPy("welovepy") == True
-    assert example.endsPy("welovepyforreal") == False
-    assert example.endsPy("pyiscool") == False
-    assert example.endsPy("hurrayforpY") == True
